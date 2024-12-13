d22c93c5b55b486ffed5a183982baceeb4f962f7
Tamanna123-nilu <tamanna786284@gmail.com>
2024-12-13 20:35:09 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -5,5 +5,12 @@ resource "azurerm_resource_group" "example" {
     name = krishna
   }
 }
+resource "azurerm_resource_group" "vm" {
+  name     = "vm"
+  location = "east us"
+  tags = {
+    name = krishna
+  }
+}
 
 
